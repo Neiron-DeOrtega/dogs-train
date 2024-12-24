90f4e6032e8d7f6e139c24c7e3615991c3fa1c49
Neiron-DeOrtega <igorpodelsk0@mail.ru>
2024-12-25 00:36:13 +0300

    add comments
    добавлены пояснительные комментарии

--- src/routers/trainingRouter.ts
+++ src/routers/trainingRouter.ts
@@ -6,8 +6,8 @@ import { tokenVerify } from "../middleware/tokenVerify"
 const express = require('express') 
 const trainingRouter = express.Router()
 
-trainingRouter.post('/submit', tokenVerify, trainingController.submit)
-trainingRouter.post('/create', tokenVerify, adminVerify, trainingController.create)
-trainingRouter.get('/confirm/:sid', tokenVerify, adminVerify, trainingController.confirm)
+trainingRouter.post('/submit', tokenVerify, trainingController.submit) // Сохрание пройденного опроса
+trainingRouter.post('/create', tokenVerify, adminVerify, trainingController.create) // Создание неподтвержденного опроса
+trainingRouter.get('/confirm/:sid', tokenVerify, adminVerify, trainingController.confirm) // Подтерждение и отправка опроса пользователям
 
 module.exports = trainingRouter
--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -6,14 +6,14 @@ import userController from "../controllers/userController"
 const express = require('express')
 const userRouter = express.Router()
 
-userRouter.post('/login', userController.login)
-userRouter.post('/create', tokenVerify, adminVerify, userController.create)
-userRouter.put('/edit', tokenVerify, adminVerify, userController.edit)
-userRouter.delete('/delete', tokenVerify, adminVerify, userController.delete)
-userRouter.get('/stats/:uid', tokenVerify, adminVerify, userController.getStats)
-userRouter.get('/:uid', tokenVerify, adminVerify, userController.getUserById)
-userRouter.get('/', tokenVerify, adminVerify, userController.getAllUsers)
-userRouter.get('/protected', tokenVerify, userController.protected)
-userRouter.get('/protected-admin', tokenVerify, adminVerify, userController.protectedAdmin)
+userRouter.post('/login', userController.login) // Авторизация
+userRouter.post('/create', tokenVerify, adminVerify, userController.create) // Создание пользователя
+userRouter.put('/edit', tokenVerify, adminVerify, userController.edit) // Редактирование информации о пользователе
+userRouter.delete('/delete', tokenVerify, adminVerify, userController.delete) // Удаление пользователя
+userRouter.get('/stats/:uid', tokenVerify, adminVerify, userController.getStats) // Получение информации о пройденных опросах пользователя
+userRouter.get('/:uid', tokenVerify, adminVerify, userController.getUserById) // Найти пользователя по ID
+userRouter.get('/', tokenVerify, adminVerify, userController.getAllUsers) // Вывод всех пользователей
+userRouter.get('/protected', tokenVerify, userController.protected) // Переход по защищенному роуту
+userRouter.get('/protected-admin', tokenVerify, adminVerify, userController.protectedAdmin) // Переход на админ роут
 
 module.exports = userRouter
